53d47a63ff5ba26521744aec879c910c12f20808
Amalya Megerman <amalyamegerman@gmail.com>
2023-01-19 15:12:46 -0500

    modify methods with regex

--- email-list-converter.ts
+++ email-list-converter.ts
@@ -10,32 +10,31 @@ to a CSV file format "First Name, Last Name, Email Address"
 class EmailListConverter {
   constructor(public fileName: string) {}
   public emailTxt: string = this.getEmailTxt();
-  public contactsArray: Contact[] = [];
-  public csvText: string = `First Name, Last Name, Email Address\n`;
+  unfilteredEmailArr: string[] = this.emailTxt.split(",");
+  // public contactsArray: Contact[] = [];
+  public csvText: string = `First Name, Last Name, Email Address,\n`;
 
   getEmailTxt(): string {
     return fs.readFileSync(`${this.fileName}`, "utf8");
   }
 
-  filterEmailArray() {
+  processContacts() {
     // each array item is a string of "FirstName LastName <email@website.com>"
-    let unfilteredEmailArr: string[] = this.emailTxt.split(",");
     const contactRegex =
       /(?<first>[\w\-]*) *(?<last>[\w\-\s]*\w)\s *<(?<email>.*)>/gm;
 
     // make each entry into a Contact object, push to contactsArray, add to CSV string for output
-    unfilteredEmailArr.forEach((entry) => {
-      let newContact: Contact = { firstName: "", lastName: "", email: "" };
-      let entryInfo = entry.match(contactRegex);
+    this.unfilteredEmailArr.forEach((entry) => {
+      //let newContact: Contact = { firstName: "", lastName: "", email: "" };
+      let entryInfoArr = entry.match(contactRegex);
 
-      if (entryInfo) {
-        newContact.firstName = entryInfo[0];
-        newContact.lastName = entryInfo[1];
-        newContact.email = entryInfo[2];
+      if (entryInfoArr) {
+        entryInfoArr.forEach((contact) => {
+          contact.replace(" ", ", ");
+          this.csvText += `${contact},\n`;
+        });
 
-        this.contactsArray.push(newContact);
-
-        this.csvText += `${newContact.firstName}, ${newContact.lastName}, ${newContact.email}\n`;
+        //this.contactsArray.push(newContact);
       } else {
         throw new Error("Cannot process contact info");
       }
@@ -47,5 +46,6 @@ class EmailListConverter {
   }
 }
 
-const newEmailList = new EmailListConverter("../email-list.txt");
+const newEmailList = new EmailListConverter("./email-list.txt");
+newEmailList.processContacts();
 newEmailList.print();
